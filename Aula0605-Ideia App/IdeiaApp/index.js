55f9708e6b7451e0a7e6319c00c95371b2592907
ferreirajoao-cps <joao.ferreira406@etec.sp.gov.br>
2024-05-06 20:36:55 -0300

    Aula0605 - Conector do Sequelize com MySQL

---
+++ Aula0605-Ideia App/IdeiaApp/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+const conn = require('./db/conn')
+
+const app = express()
+const Ideia = require('./models/Ideia')
+app.engine('handlebars', exphbs())
+
+app.set('view engine','handlebars')
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('layouts/main')
+})
+conn
+.sync({force: true})
+.then(() => {
+    app.listen(8080, () => {
+        console.log('Servidor:http://localhost:3000/')
+    })
+}).catch((err) => {
+    console.error(`erro na conexão com o banco de dados`)
+});
+
