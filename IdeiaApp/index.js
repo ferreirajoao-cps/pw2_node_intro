84443c9404096cf4a1993f4a35866a9b9e09f471
ferreirajoao-cps <joao.ferreira406@etec.sp.gov.br>
2024-06-17 20:50:18 -0300

    Aula1706 Sessão e Segurança

--- IdeiaApp/index.js
+++ IdeiaApp/index.js
@@ -24,15 +24,43 @@ app.use(
         extended: true
     })
 )
+app.use(express.json())
+app.use(
+    session({
+        name:'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'session'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true,
+        },
+    }),
+)
+app.use(flash())
 
 app.use(express.static('public'))
+app.use((re, res, next) => {
+    console.log(req.session.userid)
 
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+    next()
+})
 app.use('/ideias', ideiasRoutes)
 app.use('/', authRouter)
-conn
+
+app.get('/', IdeiaController.showIdeias)
 
 conn
-.sync({force: true})
+.sync()
 .then(() =>{
     app.listen(3000, () => {
         console.log('Servidor operando na porta local: http://127.0.0.1:3000')
